76e74850332688a46e2f6d4bcc4e28c398b56820
Ariana <arianafamiliar@hotmail.com>
2024-08-21 13:38:48 -0400

    update to include all files
    not just niftis

--- fw_add_missing_file_classifications_dataView.py
+++ fw_add_missing_file_classifications_dataView.py
@@ -40,35 +40,43 @@ view = fw.View(
 # ====== loop through projects ======
 for project in fw.projects.iter():
     proj_label = project.label
-    project = fw.projects.find_first('label='+proj_label);
-    print('PROCESSING: '+proj_label)
-    df = fw.read_view_dataframe(view, project.id) # dataframe with all files in this proj
-    if not df.empty:
-        df = df[df['file.modality']=='MR'] # filter to nifti's with no metadata
-        df_sub = df[(df['file.classification.Intent'].isnull()) & \
-                    (df['file.classification.Features'].isnull()) & \
-                    (df['file.classification.Measurement'].isnull())]
-        df_sub = df_sub[df_sub['file.type']=='nifti']
-        for index, row in df_sub.iterrows(): # loop through the nifti files
-            file_id = row['file.id']
-            file_name = row['file.name']
-            acq_id = row['acquisition.id']
-            acq_rows = df[df['acquisition.id']==acq_id]
-            acq_rows = acq_rows[acq_rows['file.id']!=file_id]
-            ## look for any existing classifications, if exist then use to update the missing file classification
-            if (not acq_rows[~acq_rows['file.classification.Intent'].isnull()].empty) | \
-                (not acq_rows[~acq_rows['file.classification.Features'].isnull()].empty) | \
-                (not acq_rows[~acq_rows['file.classification.Measurement'].isnull()].empty): # if it's not empty
-                first_row = acq_rows.iloc[0] # grab the first file in the acqusition
-                classification_in = {}
-                if first_row['file.classification.Intent']:
-                    classification_in['Intent'] = first_row['file.classification.Intent']
-                if first_row['file.classification.Features']:
-                    classification_in['Features'] = first_row['file.classification.Features']
-                if first_row['file.classification.Measurement']:
-                    classification_in['Measurement'] = first_row['file.classification.Measurement']
-                acq = fw.get_acquisition(acq_id)
-                acq.replace_file_classification(file_name, \
-                                                classification=classification_in )
-                print('UPDATED: '+('/'.join([row['subject.label'],row['session.label'],row['acquisition.label'],row['file.name']])))
-
+    if "_v2" in proj_label:
+        project = fw.projects.find_first('label='+proj_label)
+        print('PROCESSING: '+proj_label)
+        df = fw.read_view_dataframe(view, project.id) # dataframe with all files in this proj
+        if not df.empty:
+            print(df)
+            # df = df[df['file.modality']=='MR'] # filter to files with no metadata
+            df_sub = df[(df['file.classification.Intent'].isnull()) & \
+                        (df['file.classification.Features'].isnull()) & \
+                        (df['file.classification.Measurement'].isnull())]
+            # df_sub = df_sub[df_sub['file.type']=='nifti']
+            for index, row in df_sub.iterrows(): # loop through the nifti files
+                file_id = row['file.id']
+                file_name = row['file.name']
+                acq_id = row['acquisition.id']
+                acq_rows = df[df['acquisition.id']==acq_id]
+                acq_rows = acq_rows[acq_rows['file.id']!=file_id]
+                modalities = acq_rows['file.modality'].drop_duplicates().tolist()
+                if '' in modalities:
+                    modalities.remove('')
+                ## look for any existing classifications, if exist then use to update the missing file classification
+                if (not acq_rows[~acq_rows['file.classification.Intent'].isnull()].empty) | \
+                    (not acq_rows[~acq_rows['file.classification.Features'].isnull()].empty) | \
+                    (not acq_rows[~acq_rows['file.classification.Measurement'].isnull()].empty): # if it's not empty
+                    print('UPDATING: '+('/'.join([row['subject.label'],row['session.label'],row['acquisition.label'],row['file.name']])))
+                    first_row = acq_rows.iloc[0] # grab the first file in the acqusition
+                    classification_in = {}
+                    if first_row['file.classification.Intent']:
+                        classification_in['Intent'] = first_row['file.classification.Intent']
+                    if first_row['file.classification.Features']:
+                        classification_in['Features'] = first_row['file.classification.Features']
+                    if first_row['file.classification.Measurement']:
+                        classification_in['Measurement'] = first_row['file.classification.Measurement']
+                    acq = fw.get_acquisition(acq_id)
+                    try:
+                        acq.replace_file_classification(file_name, \
+                                                        classification=classification_in, \
+                                                        modality=modalities[0] )
+                    except:
+                        continue
